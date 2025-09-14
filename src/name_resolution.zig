9a91ade22554ecabc145182220296ce3143bf4ad
Andrew Kralovec <andrew.kralovec@yum.com>
2025-09-14 08:51:49 -0500

    fix: add blank nr

--- src/name_resolution.zig
+++ src/name_resolution.zig
@@ -1,1 +1,9 @@
-// TODO: Implement this file
+// TODO: Implement the name_resolution port from dbgrs\src\name_resolution.rs
+const Process = @import("./process.zig").Process;
+
+// Resolve symbol name to address
+fn resolveNameToAddress(sym: []const u8, process: *Process) !u64 {
+    _ = sym;
+    _ = process;
+    return error.NotImplemented;
+}
